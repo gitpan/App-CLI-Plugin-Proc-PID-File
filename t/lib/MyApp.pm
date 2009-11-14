bd2e111985c658fe982bdd3f28907559d9d5b00d
Akira Horimoto <emperor.kurt@gmail.com>
2009-11-14 15:25:29 +0000

    Import of HOLLY/App-CLI-Plugin-Proc-PID-File-0.1 from CPAN.
    gitpan-cpan-distribution: App-CLI-Plugin-Proc-PID-File
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         HOLLY/App-CLI-Plugin-Proc-PID-File-0.1.tar.gz
    gitpan-cpan-author:       HOLLY
    gitpan-cpan-maturity:     released

---
+++ t/lib/MyApp.pm
@@ -0,0 +1,17 @@
+package MyApp;
+
+use strict;
+use File::Spec;
+use base qw(App::CLI::Extension);
+use constant alias => (
+                 objcan     => "LogObjCan",
+                 logmessage    => "LogMessage",
+                 stringfystack => "StringfyStack",
+             );
+
+__PACKAGE__->config(proc_pid_file => { dir => File::Spec->tmpdir, name => "prove" });
+__PACKAGE__->load_plugins(qw(
+                 Proc::PID::File
+            ));
+
+1;
